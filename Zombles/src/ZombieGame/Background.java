3ae41af50930d62f0c9a2378f385fc25c48b0366
benc993 <ben.c993@yahoo.com>
2016-12-03 21:06:59 -0800

    Update Background.java

--- Zombles/src/ZombieGame/Background.java
+++ Zombles/src/ZombieGame/Background.java
@@ -1,10 +1,17 @@
 package ZombieGame;
 
+import javafx.scene.layout.Pane;
+import ZombieGame.Bullet;
+import ZombieGame.Player;
+import ZombieGame.Shoot;
+import ZombieGame.Zombies;
+
 public class Background {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Pane pane = new Pane();
+		
 	}
 
 }
