aa45ae29a84281516ac4f59844ffbafe0d319508
benc993 <ben.c993@yahoo.com>
2016-12-03 21:07:28 -0800

    Update Bullet.java

--- Zombles/src/ZombieGame/Bullet.java
+++ Zombles/src/ZombieGame/Bullet.java
@@ -1,10 +1,17 @@
 package ZombieGame;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
 public class Bullet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Rectangle bullet = new Rectangle (4, 4);
+		bullet.setStroke(Color.BLACK);
+		bullet.setFill(Color.YELLOW);
+		
 	}
 
 }
